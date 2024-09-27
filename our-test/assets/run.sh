9c64f4de429a93ec5832d61c8360663554f93052
Hampus Hallkvist <hampus@hallkvist.org>
2024-09-27 15:09:49 +0200

    fix: update run.sh
    Co-authored-by: Melvin Jakobsson <blytung01@gmail.com>

--- our-test/assets/run.sh
+++ our-test/assets/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && source ~/.bashrc
 nvm install 20 -y
 nvm use 20 -y
 echo "All done, yeay!"
