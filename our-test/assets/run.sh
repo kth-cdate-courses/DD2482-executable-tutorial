4062cd9bc560856c79692022fc1a00b08f8295b8
Hampus Hallkvist <hampus@hallkvist.org>
2024-09-27 15:13:55 +0200

    fix: Add final npm install to foreground task

--- our-test/assets/run.sh
+++ our-test/assets/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Installing Node.js 20..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && source ~/.bashrc
 nvm install 20 -y
 nvm use 20 -y
+
+echo "Installting dependencies..."
+cd server/app && npm install
+
 echo "All done, yeay!"
