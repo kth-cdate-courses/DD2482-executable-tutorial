401ddaa0588b8cb912f869a6fef784a159b14f90
Hampus Hallkvist <hampus@hallkvist.org>
2024-09-27 15:04:55 +0200

    fix: update run.sh
    Co-authored-by: Melvin Jakobsson <blytung01@gmail.com>

--- our-test/assets/run.sh
+++ our-test/assets/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-apt install node -y 
-apt install npm -y 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install 20 -y
+nvm use 20 -y
 echo "All done, yeay!"
