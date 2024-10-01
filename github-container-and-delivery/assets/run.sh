02b6e9bc0aa866d42fb306d8d716e56f462c759c
Hampus Hallkvist <hampus@hallkvist.org>
2024-10-01 15:11:33 +0200

    fix: Change run.sh echos

--- github-container-and-delivery/assets/run.sh
+++ github-container-and-delivery/assets/run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "Installing Node.js 20..."
+@echo "Installing Node.js 20..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && source ~/.bashrc
 nvm install 20 -y
 nvm use 20 -y
 
-echo "Installting dependencies..."
+@echo "Installting dependencies..."
 cd server/app && npm install
+clear 
 
 # Create incomplete files
 touch server/Dockerfile
+mkdir -p server/.github/workflows
 touch server/.github/workflows/deploy.yml
 
-echo "All done!"
-echo "You'll be working in two files: `Dockerfile` and `.github/workflows/deploy.yml`"
+@echo "All done!"
+@echo "You'll be working in two files: 'Dockerfile' and '.github/workflows/deploy.yml'"
