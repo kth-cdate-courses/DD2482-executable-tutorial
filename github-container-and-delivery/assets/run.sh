8ea503351ab5981d7f2e73418e66b24d8855c01c
Hampus Hallkvist <hampus@hallkvist.org>
2024-10-01 15:13:00 +0200

    fix: disable echos attempt #2

--- github-container-and-delivery/assets/run.sh
+++ github-container-and-delivery/assets/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-@echo "Installing Node.js 20..."
+# Disable command echoing
+set +x
+
+echo "Installing Node.js 20..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash && source ~/.bashrc
 nvm install 20 -y
 nvm use 20 -y
 
-@echo "Installting dependencies..."
+echo "Installting dependencies..."
 cd server/app && npm install
 clear 
 
@@ -14,5 +17,5 @@ touch server/Dockerfile
 mkdir -p server/.github/workflows
 touch server/.github/workflows/deploy.yml
 
-@echo "All done!"
-@echo "You'll be working in two files: 'Dockerfile' and '.github/workflows/deploy.yml'"
+echo "All done!"
+echo "You'll be working in two files: 'Dockerfile' and '.github/workflows/deploy.yml'"
